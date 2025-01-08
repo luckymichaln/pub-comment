75e4c89d3d89c7ac5029cc9745f3a5e68f121ca5
Michal Nowak <michalnowak.contact@gmail.com>
2025-01-08 22:13:10 +0100

    add feedback form and list components

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
+import { MAX_CHARACTERS } from "../lib/constans";
+
 export const FeedbackForm = () => {
+  const [text, setText] = useState("");
+  const charLeft = MAX_CHARACTERS - text.length;
+
+  const handleOnChange = (ev: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = ev.target.value;
+    if (newText.length > MAX_CHARACTERS) return;
+    setText(ev.target.value);
+  };
+
   return (
     <form className="form">
       <textarea
         name="feedback-textarea"
         id="feedback-textarea"
+        onChange={handleOnChange}
         placeholder="Placeholder"
         spellCheck={false}
       />
@@ -11,7 +24,7 @@ export const FeedbackForm = () => {
         Enter your feedback here, remember to #hashtag the company
       </label>
       <div className="">
-        <p className="u-italic">150</p>
+        <p className="u-italic">{charLeft}</p>
         <button>
           <span>Submit</span>
         </button>
--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,3 +1,46 @@
+import { useCallback, useEffect, useState } from "react";
+import { FeedbackItem } from "./FeedbackItem";
+import { API_URL } from "../lib/constans";
+import { Spinner } from "./Spinner";
+import { ErrorMessage } from "./ErrorMessage";
+
 export const FeedbackList = () => {
-  return <ol className="feedback-list">FeedbackList</ol>;
+  const [feedbackItems, setFeedbackItems] = useState<FeedbackItem[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const fetchData = useCallback(async () => {
+    setIsLoading(true);
+
+    try {
+      const result = await fetch(`${API_URL}/feedbacks`);
+      if (!result.ok) {
+        throw new Error();
+      }
+
+      const { feedbacks } = await result.json();
+      setFeedbackItems(feedbacks);
+      setIsLoading(false);
+    } catch (error) {
+      setErrorMessage("Something went wrong.");
+    }
+
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return (
+    <ul className="feedback-list">
+      {isLoading ? <Spinner /> : null}
+
+      {errorMessage ? <ErrorMessage message={errorMessage} /> : null}
+
+      {feedbackItems.map((item) => {
+        return <FeedbackItem key={item.id} feedbackItem={item} />;
+      })}
+    </ul>
+  );
 };
