7fd1e3c7e2313cf3e350bfd57fbd3cf6e3aaecb1
Michal Nowak <michalnowak.contact@gmail.com>
2025-01-14 15:00:15 +0100

    add simple form validation

--- src/components/feedback/FeedbackForm.tsx
+++ src/components/feedback/FeedbackForm.tsx
@@ -7,6 +7,8 @@ type FeedbackFormProps = {
 
 export const FeedbackForm = ({ onAddToList }: FeedbackFormProps) => {
   const [text, setText] = useState("");
+  const [isFormValid, setIsFormValid] = useState(false);
+  const [isFormInvalid, setIsFormInvalid] = useState(false);
   const charLeft = MAX_CHARACTERS - text.length;
 
   const handleOnChange = (ev: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -18,12 +20,27 @@ export const FeedbackForm = ({ onAddToList }: FeedbackFormProps) => {
   const handleSubmit = (ev: FormEvent<HTMLFormElement>) => {
     ev.preventDefault();
 
+    if (text.includes("#") && text.length >= 5) {
+      setIsFormValid(true);
+
+      setTimeout(() => setIsFormValid(false), 500);
+    } else {
+      setIsFormInvalid(true);
+      setTimeout(() => setIsFormInvalid(false), 500);
+      return;
+    }
+
     onAddToList(text);
     setText("");
   };
 
   return (
-    <form onSubmit={handleSubmit} className="form">
+    <form
+      onSubmit={handleSubmit}
+      className={`form ${isFormValid ? "form--valid" : ""} ${
+        isFormInvalid ? "form--invalid" : ""
+      }`}
+    >
       <textarea
         name="feedback-textarea"
         id="feedback-textarea"
